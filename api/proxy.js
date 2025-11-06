74ec37a3b0c79decf6bab0a03c0b1aa4d03b95e4
小白 <121837289+cunzhong@users.noreply.github.com>
2025-11-06 17:44:10 +0800

    更新 proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@
 // 域名映射配置
 const domain_mappings = {
   'youtube.com': 'gh-y.',
-  'fbi.15896779059.workera.dev': 'gh-f.',
+  'fbi.15896779059.workers.dev': 'gh-f.',
   'github.com': 'gh.',
   'avatars.githubusercontent.com': 'gh-avatars-githubusercontent-com.',
   'github.githubassets.com': 'gh-github-githubassets-com.',
